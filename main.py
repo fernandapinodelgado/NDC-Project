f2b9587eeeff3dcb287bfe7b709fb9e431c7114d
Colin Lemarchand <clemarch@ucsd.edu>
2021-01-16 18:33:13 -0800

    slight refactoring

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 ndc_html_dir = 'html/'
 ndc_txt_dir = 'txt/'
 
-for ndc_name in os.listdir('html/'):
-    with open(ndc_html_dir + ndc_name) as ndc_file:
+for ndc_filename in os.listdir(ndc_html_dir):
+    with open(ndc_html_dir + ndc_filename) as ndc_file:
         ndc_content = ndc_file.read()
         soup = BeautifulSoup(ndc_content, 'lxml')
-        with open(ndc_txt_dir + ndc_name.split('.')[0] + '.txt', 'w') as outfile:
+        with open(ndc_txt_dir + ndc_filename.split('.')[0] + '.txt', 'w') as outfile:
             outfile.write(soup.text)
