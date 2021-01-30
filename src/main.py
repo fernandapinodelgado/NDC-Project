b49344f239b2ada12c1274e4a280ced3ff815329
Colin Lemarchand <clemarch@ucsd.edu>
2021-01-30 14:11:47 -0800

    added EN filter option to extract_a_ids

--- src/main.py
+++ src/main.py
@@ -15,13 +15,13 @@ def html_to_txt(html_dir, txt_dir):
                 outfile.write(soup.text)
 
 
-def extract_a_ids(html_dir, a_id_csv):
+def extract_a_ids(html_dir, a_id_csv, filter_EN):
     with open(a_id_csv, 'w') as a_id_file:
         for ndc_filename in os.listdir(html_dir):
             with open(html_dir + ndc_filename) as ndc_file:
                 ndc_content = ndc_file.read()
                 ndc_file_abrev = ndc_filename.split("-")[0]
-                if ndc_filename.split("-")[2].split(".")[0] != "EN":
+                if filter_EN and ndc_filename.split("-")[2].split(".")[0] != "EN":
                     continue
                 soup = BeautifulSoup(ndc_content, 'lxml')
                 a_tags = soup.find_all("a")
@@ -32,5 +32,5 @@ def extract_a_ids(html_dir, a_id_csv):
             
 
 if __name__ == "__main__":
-    extract_a_ids(ndc_html_dir, "../csv/A-IDs.csv")
+    extract_a_ids(ndc_html_dir, "../csv/A-IDs.csv", True)
     os.system("sort ../csv/A-IDs.csv > ../csv/sorted-A-IDs.csv")
